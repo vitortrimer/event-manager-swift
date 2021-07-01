78318393027f664622b4906fa4c882981ddfabd9
Vitor Trimer <vitortrimer@gmail.com>
2021-07-01 19:56:02 -0300

    Add empty project

---
+++ EventManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventManager
+//
+//  Created by Vitor Trimer on 01/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
